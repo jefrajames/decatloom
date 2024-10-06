6943fd07c7c9e044e6476642afbe314379c5d3b8
Jean-Francois James <jefrajames@gmail.com>
2024-10-06 22:47:01 +0200

    Minor updates

--- liberty/setEnv.sh
+++ liberty/setEnv.sh
@@ -1,7 +1,11 @@
 #! /bin/zsh
 
 # WRK connections (injector)
-export WRK_C=400
+#export WRK_C=800
+#export WRK_C=1600
+export WRK_C=3200
 
 # Server Database Connection Pool Size
-export DB_POOL_SIZE=200
+#export DB_POOL_SIZE=200
+#export DB_POOL_SIZE=400
+export DB_POOL_SIZE=800
--- quarkus/setEnv.sh
+++ quarkus/setEnv.sh
@@ -4,8 +4,8 @@
 export WRK_C=800
 
 # Endpoint: determines the thread type
-export SERVER_THREAD=platform
-#export SERVER_THREAD=virtual
+#export SERVER_THREAD=platform
+export SERVER_THREAD=virtual
 
 # Server Quarkus Thread Pool Size (only for Platform Threads)
 export QUARKUS_THREAD_POOL_CORE_THREADS=400
